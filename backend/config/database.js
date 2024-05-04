aed37bfbcb8349bb486498657321451595068831
Aashu623 <aashukushwah53678@gmail.com>
2024-05-04 20:55:06 +0530

    update mongo url

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.MONGO_URL).then(() => {
+    mongoose.connect('mongodb+srv://aashukushwah53678:aQFWh3hTbPKlZMI5@ecommerce.mduns4t.mongodb.net/Ecommerce?retryWrites=true&w=majority&appName=Ecommerce').then(() => {
         console.log(`Mongodb connected with server :${mongoose.connection.host}`);
     }); 
 }
