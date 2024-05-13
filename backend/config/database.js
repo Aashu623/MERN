88b653591de16fae991e89d54878b8d9899cc633
Aashu623 <aashukushwah53678@gmail.com>
2024-05-13 14:51:43 +0530

    fixed bugs

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const connectDatabase = () => {
-    mongoose.connect('mongodb+srv://aashukushwah53678:aQFWh3hTbPKlZMI5@ecommerce.mduns4t.mongodb.net/Ecommerce?retryWrites=true&w=majority&appName=Ecommerce').then(() => {
+    mongoose.connect(process.env.MONGO_URL).then(() => {
         console.log(`Mongodb connected with server :${mongoose.connection.host}`);
-    }); 
+    });
 }
 
 module.exports = connectDatabase;
