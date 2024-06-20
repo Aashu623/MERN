4cd1d932282767aee4c393cec63eb0b56975516a
Aashu623 <aashukushwah53678@gmail.com>
2024-06-20 15:07:59 +0530

    token bug fixed

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,6 +9,7 @@ const sendToken = (user, statusCode, res) => {
         ),
         httpOnly: true,
     };
+    req.user = user;
 
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
