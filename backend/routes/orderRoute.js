5e07af6479f83989eb49400e1d7528a26d8de758
Aashu623 <aashukushwah53678@gmail.com>
2024-06-20 15:19:11 +0530

    delete middleware from routes

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+const { authorizeRoles } = require("../middleware/auth");
 const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder } = require('../controllers/orderController');
 
-router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/new").post(newOrder);
 
-router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
+router.route('/order/:id').get(getSingleOrder);
 
-router.route('/orders/me').get(isAuthenticatedUser, myOrders);
+router.route('/orders/me').get(myOrders);
 
 router
     .route('/admin/orders')
-    .get(isAuthenticatedUser, authorizeRoles('admin'), getAllOrders)
+    .get(authorizeRoles('admin'), getAllOrders)
 
 router
     .route('/admin/order/:id')
-    .put(isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
-    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteOrder);
+    .put(authorizeRoles('admin'), updateOrder)
+    .delete(authorizeRoles('admin'), deleteOrder);
 module.exports = router;
