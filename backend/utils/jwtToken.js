9796117a2b11e6ea4ef5ceed70fdf5abb3782ea2
Aashu623 <aashukushwah53678@gmail.com>
2024-07-01 16:28:16 +0530

    set secure cookie true

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,7 +7,7 @@ const sendToken = (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+        secure: true, // Use secure cookies in production
         sameSite: 'None' // Ensures the cookie is sent in cross-site requests
     };
 
