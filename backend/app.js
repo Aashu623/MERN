f995a648ff278d2cc09784ab7fd9c055813c3624
Aashish Kushwah <aashukushwah53678@gmail.com>
2025-08-18 23:15:03 +0530

    Update cors

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,58 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors')
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
-const errorMiddleware = require('./middleware/error')
-const path = require('path')
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const errorMiddleware = require("./middleware/error");
+const path = require("path");
 
 // config
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config({ path: 'backend/config/config.env' })
+  require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-app.use(cors({
-    origin: 'https://mernfrontend-tau.vercel.app',
-    credentials: true
-}));
+const allowedOrigins = [
+  "https://mernfrontend-tau.vercel.app",
+  "https://www.brahmiacademy.com",
+  "https://brahmiacademy.com",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-
 
 // app.use((req, res, next) => {
 //     res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
 //     next();
 // });
 
-
 //Route Imports
-const product = require('./routes/productRoute');
-const user = require('./routes/userRoute')
-const order = require('./routes/orderRoute');
-const payment = require('./routes/paymentRoute');
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
 
-app.use('/api/v1', product);
-app.use('/api/v1', user);
-app.use('/api/v1', order);
-app.use('/api/v1', payment);
-
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
 
 // app.use(express.static(path.join(__dirname, '../frontend/build')))
 
